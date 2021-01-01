a9bf4e230f104d0141f4ddc9f067b64956a09d5c
Ayaz Duru <ayzdru@outlook.com>
2021-01-01 22:28:50 +0300

    ValueConverter düzenlendi.

--- source/AspNetEntityFrameworkDataEncryption/AspNetEntityFrameworkDataEncryption/Data/ApplicationDbContext.cs
+++ source/AspNetEntityFrameworkDataEncryption/AspNetEntityFrameworkDataEncryption/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace AspNetEntityFrameworkDataEncryption.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public static ValueConverter<string,string> EncryptionValueConverter { get; set; } = new ValueConverter<string, string>(
+        private readonly static ValueConverter<string,string> _encryptionValueConverter = new ValueConverter<string, string>(
     v => AesEncryptionExtension.Encrypt(v),
     v => AesEncryptionExtension.Decrypt(v));
         public DbSet<Person> Persons { get; set; }
@@ -24,8 +24,8 @@ namespace AspNetEntityFrameworkDataEncryption.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            builder.Entity<Person>().Property(p => p.Identity).HasConversion(EncryptionValueConverter);
-            builder.Entity<Person>().Property(p => p.PhoneNumber).HasConversion(EncryptionValueConverter);
+            builder.Entity<Person>().Property(p => p.Identity).HasConversion(_encryptionValueConverter);
+            builder.Entity<Person>().Property(p => p.PhoneNumber).HasConversion(_encryptionValueConverter);
             builder.Entity<Person>().HasData(
    new Person() { Id = Guid.NewGuid(), Identity = "8248649570", FirstName =  "Ayaz", LastName = "Duru", PhoneNumber = "5554442121" });
         }
